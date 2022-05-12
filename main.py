27eaacaf86e994c1854a998305cc14bc96a22099
Benjamin Mirisola <bennymirisola@gmail.com>
2022-05-12 00:34:20 -0400

    amazing and also got rid of aspect ratio

--- main.py
+++ main.py
@@ -37,19 +37,23 @@ while (1):
     # Set range for blue color and
     # define mask
     # B G R of HSV
-    color_lower = np.array([100, 130, 85], np.uint8)
-    color_upper = np.array([120, 215, 215], np.uint8)
+    color_lower = np.array([107, 115, 78], np.uint8)
+    color_upper = np.array([132, 240, 240], np.uint8)
     mask = cv2.inRange(hsvFrame, color_lower, color_upper)
 
     # Morphological Transform, Dilation
     # for each color and bitwise_and operator
     # between imageFrame and mask determines
     # to detect only that particular color
-    kernel = np.ones((10, 10), "uint8")
+    kernel = np.ones((5, 5), "uint8")
 
     # For blue color
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    #mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    #mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+    # Erosion then dilation
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    mask = cv2.dilate(src=mask,kernel=kernel)
+    #cv2.erode(mask,kernel)
     res_green = cv2.bitwise_and(imageFrame, imageFrame, mask=mask)
 
     # Creating contour to track green color
@@ -59,25 +63,26 @@ while (1):
 
     for pic, contour in enumerate(contours):
         area = cv2.contourArea(contour)
-        if (area > 12000):
+        if (area > 500):
             x, y, w, h = cv2.boundingRect(contour)
             aspect_ratio = float(w) / h
             #print(aspect_ratio)
             # @TODO Better implement aspect ratio
-            if aspect_ratio <= 1000:
-                imageFrame = cv2.rectangle(imageFrame, (x, y),
-                                           (x + w, y + h),
-                                           (0, 255, 0), 2)
 
-                cv2.putText(imageFrame, "Frisbee", (x, y),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            1.0, (0, 255, 0))
-                center = [x+w/2,y+h/2]
+            imageFrame = cv2.rectangle(imageFrame, (x, y),
+                                       (x + w, y + h),
+                                       (0, 255, 0), 2)
+
+            cv2.putText(imageFrame, "Frisbee", (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1.0, (0, 255, 0))
+            center = [x+w/2,y+h/2]
+            print(center)
 
 
 
     # Program Termination
-    cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame)
+    cv2.imshow("Color Detector", imageFrame)
     cv2.imshow("mask",mask)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         webcam.release()
