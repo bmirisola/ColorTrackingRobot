ee8290ad066e8c163910650405ec981f1c5c4c45
Benjamin Mirisola <bennymirisola@gmail.com>
2022-05-14 23:19:38 -0400

    added Tuner methods for hsv to binary

--- Tuner.py
+++ Tuner.py
@@ -1,34 +1,34 @@
 import numpy as np
 import cv2
-from test import test_constants
+import Constants
 
-def on_change(val=0):
-    color_upper = np.array([cv2.getTrackbarPos(test_constants.h_upper,windowName),
-                            cv2.getTrackbarPos(test_constants.s_upper,windowName),
-                            cv2.getTrackbarPos(test_constants.v_upper,windowName)]
-                           , np.uint8)
-    color_lower = np.array([cv2.getTrackbarPos(test_constants.h_lower,windowName),
-                            cv2.getTrackbarPos(test_constants.s_lower,windowName),
-                            cv2.getTrackbarPos(test_constants.v_lower,windowName)],
-                           np.uint8)
-    mask = cv2.inRange(hsv,color_lower,color_upper)
-    cv2.imshow('mask',mask)
+class Tuner:
+
+    def __int__(self, windowName):
+        self.windowName = windowName
+        self._mask = []
+
+        cv2.createTrackbar(Constants.h_upper, windowName, 0, 255, self.__hsvTrackBarUpdate)
+        cv2.createTrackbar(Constants.s_upper, windowName, 0, 255, self.__hsvTrackBarUpdate)
+        cv2.createTrackbar(Constants.v_upper, windowName, 0, 255, self.__hsvTrackBarUpdate)
+
+        cv2.createTrackbar(Constants.h_lower, windowName, 0, 255, self.__hsvTrackBarUpdate)
+        cv2.createTrackbar(Constants.s_lower, windowName, 0, 255, self.__hsvTrackBarUpdate)
+        cv2.createTrackbar(Constants.v_lower, windowName, 0, 255, self.__hsvTrackBarUpdate)
 
 
-img = cv2.imread('/home/bmirisola/PycharmProjects/ColorTrackingRobot/test/test.jpg')
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-windowName = 'image'
-cv2.namedWindow(windowName)
+    def __hsvTrackBarUpdate(self, hsv):
 
-cv2.createTrackbar('h_upper', windowName, 0, 255, on_change)
-cv2.createTrackbar('s_upper', windowName, 0, 255, on_change)
-cv2.createTrackbar('v_upper', windowName, 0, 255, on_change)
+        color_upper = np.array([cv2.getTrackbarPos(Constants.h_upper, self.windowName),
+                                cv2.getTrackbarPos(Constants.s_upper, self.windowName),
+                                cv2.getTrackbarPos(Constants.v_upper, self.windowName)]
+                               , np.uint8)
+        color_lower = np.array([cv2.getTrackbarPos(Constants.h_lower, self.windowName),
+                                cv2.getTrackbarPos(Constants.s_lower, self.windowName),
+                                cv2.getTrackbarPos(Constants.v_lower, self.windowName)],
+                               np.uint8)
+        self.mask = cv2.inRange(hsv, color_lower, color_upper)
 
-cv2.createTrackbar('h_lower', windowName, 0, 255, on_change)
-cv2.createTrackbar('s_lower', windowName, 0, 255, on_change)
-cv2.createTrackbar('v_lower', windowName, 0, 255, on_change)
 
-cv2.imshow(windowName, img)
-on_change()
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    def updateBinary(self):
+        cv2.imshow(self.mask)
